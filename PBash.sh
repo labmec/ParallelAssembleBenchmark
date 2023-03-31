1dde94291112bfe2fe2249f9f13b82fad927bbb3
Bahamasman <marcoalopes99@gmail.com>
2023-03-31 16:10:00 -0300

    Couple of First Tests

--- PBash.sh
+++ PBash.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
-echo $"Resultados para TBB" >> Results.txt
+echo $"Resultados para STD" >> Results.txt
 #bash for loop
-/usr/bin/cmake --build build --config Debug --target pbench -j 14 --
+cmake --build ParallelAssembleBenchmark_build/ --config Debug --target pbench -j 14 --
 for a in 2 4 8 12 16 24 32 64
 do
 echo $a > PTests.txt
 echo Number of threads: $a
 echo Number of threads: $a >>  Results.txt
-cd build;./pbench;cd ..
+cd ParallelAssembleBenchmark_build/;./pbench;cd ..
 done
 
 
--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ using namespace std;
 
 
 enum EMatid {EDomain, EBC};
-//#define USE_STD
-#define USE_TBB
+#define USE_STD
+//#define USE_TBB
 //#define USE_OMP
 
 int main (int argc, char * const argv[]) {
@@ -36,14 +36,13 @@ if (textfile.is_open()){ //checking whether the file is open
      threadnum =  stoi(tp);
   }
   textfile.close(); //close the file object.
-  }
-  
+  }  
   const bool isSolveAndPostProc = false;
 
   // Create geometric mesh
   const TPZVec<REAL> minX = {-1.,-1.,-1.};
   const TPZVec<REAL> maxX = {1.,1.,1.};
-  const int ndivperside = 5;
+  const int ndivperside = 15;
   const TPZVec<int> nelDiv = {ndivperside,ndivperside,ndivperside};
   const MMeshType elType = MMeshType::EHexahedral;
   TPZGenGrid3D gen3d(minX,maxX,nelDiv,elType);
@@ -57,7 +56,7 @@ if (textfile.is_open()){ //checking whether the file is open
   }
   
   // Create computational mesh
-  const int pOrder = 2;
+  const int pOrder = 3;
   TPZHDivApproxCreator hdivCreator(gmesh);
   hdivCreator.SetDefaultOrder(pOrder);
   hdivCreator.ProbType() = ProblemType::EDarcy;
